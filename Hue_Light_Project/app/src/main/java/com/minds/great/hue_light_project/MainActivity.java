41ef6d30144e4f946fda492b100ced28979405a2
darthpippin <sarah.sweimler@gmail.com>
2017-10-04 22:39:16 -0400

    Finishes 'Search Indicator'; Adds annotations

--- Hue_Light_Project/app/src/main/java/com/minds/great/hue_light_project/MainActivity.java
+++ Hue_Light_Project/app/src/main/java/com/minds/great/hue_light_project/MainActivity.java
@@ -2,12 +2,30 @@ package com.minds.great.hue_light_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+
+    @ViewById
+    View searchProgressBar;
+    @ViewById
+    View searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Click(resName = "searchButton")
+    public void searchForBridges(){
+        searchProgressBar.setVisibility(View.VISIBLE);
+        searchButton.setVisibility(View.GONE);
+    }
+
 }
