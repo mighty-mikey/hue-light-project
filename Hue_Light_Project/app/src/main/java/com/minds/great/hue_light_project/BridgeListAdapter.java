53fe86ffe4c104a2928414117c7766c3532705ef
darthpippin <sarah.sweimler@gmail.com>
2017-11-14 22:30:52 -0500

    WIP -- Adds formatting to bridge list; adds dividers to list.

--- Hue_Light_Project/app/src/main/java/com/minds/great/hue_light_project/BridgeListAdapter.java
+++ Hue_Light_Project/app/src/main/java/com/minds/great/hue_light_project/BridgeListAdapter.java
@@ -1,4 +1,5 @@
 package com.minds.great.hue_light_project;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -6,12 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
 import com.philips.lighting.hue.sdk.PHAccessPoint;
+
 import java.util.List;
 
-public class BridgeListAdapter extends BaseAdapter{
+public class BridgeListAdapter extends BaseAdapter {
     private List<PHAccessPoint> listOfFoundBridges;
     private Context context;
+    final private int HEADER_CODE = 1;
+    final private int FOOTER_CODE = -1;
 
     @Override
     public int getCount() {
@@ -34,15 +39,34 @@ public class BridgeListAdapter extends BaseAdapter{
         LayoutInflater inflater = (LayoutInflater) context.
                 getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+
         @SuppressLint("ViewHolder") View itemView = null;
+        int itemViewType = getItemViewType(position);
+
         if (inflater != null) {
-            itemView = inflater.inflate(R.layout.bridge_list_item, null);
+            if (itemViewType == HEADER_CODE) {
+                itemView = inflater.inflate(R.layout.bridge_list_header, null);
+            } else if (itemViewType == FOOTER_CODE) {
+                itemView = inflater.inflate(R.layout.bridge_list_footer, null);
+            } else {
+                itemView = inflater.inflate(R.layout.bridge_list_item, null);
+            }
             TextView bridgeName = (TextView) itemView.findViewById(R.id.bridgeName);
             TextView macAddress = (TextView) itemView.findViewById(R.id.bridgeMacAddress);
             bridgeName.setText(listOfFoundBridges.get(position).getBridgeId());
             macAddress.setText(listOfFoundBridges.get(position).getMacAddress());
         }
         return itemView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return HEADER_CODE;
+        } else if (position == listOfFoundBridges.size() - 1) {
+            return FOOTER_CODE;
+        }
+        return 0;
     }
 
     void setBridgeList(List<PHAccessPoint> listOfFoundBridges, Context context) {
