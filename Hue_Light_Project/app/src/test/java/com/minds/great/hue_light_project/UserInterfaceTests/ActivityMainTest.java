0e732d85c5526da641c318bf9681b42557bd477c
mighty_mikey <michaelklimcho@gmail.com>
2017-11-30 21:18:00 -0500

    WIP -- formatting

--- Hue_Light_Project/app/src/test/java/com/minds/great/hue_light_project/UserInterfaceTests/ActivityMainTest.java
+++ Hue_Light_Project/app/src/test/java/com/minds/great/hue_light_project/UserInterfaceTests/ActivityMainTest.java
@@ -1,14 +1,16 @@
-package com.minds.great.hue_light_project.userInterfaceTests;
+package com.minds.great.hue_light_project.UserInterfaceTests;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(RobolectricTestRunner.class)
 public class ActivityMainTest {
 
     @Test
     public void test(){
-        assert(true);
+        assertThat(true).isTrue();
     }
 }
