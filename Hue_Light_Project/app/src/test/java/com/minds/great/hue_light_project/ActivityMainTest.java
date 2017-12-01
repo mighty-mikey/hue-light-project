a2686f4dc26c5ded321d7d2979d061a1b25e0cb2
mighty_mikey <michaelklimcho@gmail.com>
2017-11-30 20:53:52 -0500

    WIP -- Set up initial robolectric tests.

---
+++ Hue_Light_Project/app/src/test/java/com/minds/great/hue_light_project/ActivityMainTest.java
@@ -0,0 +1,16 @@
+package com.minds.great.hue_light_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class ActivityMainTest {
+
+    @Test
+    public void test(){
+        assert(true);
+    }
+}
