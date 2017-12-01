b7465d5d2e1d7d731f6629260837e2fc258f605d
mighty_mikey <michaelklimcho@gmail.com>
2017-11-30 21:48:23 -0500

    Finishes robolectric set up.

--- Hue_Light_Project/app/src/test/java/com/minds/great/hue_light_project/UserInterfaceTests/ActivityMainTest.java
+++ Hue_Light_Project/app/src/test/java/com/minds/great/hue_light_project/UserInterfaceTests/ActivityMainTest.java
@@ -1,16 +1,41 @@
 package com.minds.great.hue_light_project.UserInterfaceTests;
 
+import com.minds.great.hue_light_project.BuildConfig;
+import com.minds.great.hue_light_project.MainActivity;
+import com.minds.great.hue_light_project.R;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.robolectric.annotation.Config;
+import static junit.framework.Assert.assertNotNull;
 
 
 @RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ActivityMainTest {
 
+
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+
+
     @Test
-    public void test(){
-        assertThat(true).isTrue();
+    public void mainActivity_shouldNotBeNull() throws Exception {
+        assertNotNull(activity);
+    }
+
+    @Test
+    public void mainActivity_shouldHaveSearchButton() throws Exception {
+        assertNotNull(activity.findViewById(R.id.searchButton));
     }
 }
